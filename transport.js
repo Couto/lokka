e448d0bc633b0ef0f1bfb70379536e8120592524
Luis Couto <luis.couto@mindera.com>
2016-08-03 14:58:17 +0100

    Output to UMD instead of CommonJS

--- transport.js
+++ transport.js
@@ -1,36 +1,46 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'babel-runtime/helpers/classCallCheck', 'babel-runtime/helpers/createClass'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('babel-runtime/helpers/classCallCheck'), require('babel-runtime/helpers/createClass'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.classCallCheck, global.createClass);
+    global.transport = mod.exports;
+  }
+})(this, function (exports, _classCallCheck2, _createClass2) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
-var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+  var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+  var _createClass3 = _interopRequireDefault(_createClass2);
 
-var _createClass2 = require('babel-runtime/helpers/createClass');
-
-var _createClass3 = _interopRequireDefault(_createClass2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var LokkaTransport = function () {
-  function LokkaTransport() {
-    (0, _classCallCheck3.default)(this, LokkaTransport);
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
   }
 
-  (0, _createClass3.default)(LokkaTransport, [{
-    key: 'send',
-
-    /*eslint-disable */
-    value: function send(rawQuery, variables, operationName) {
-      throw new Error('not implemented!');
-      // return new Promise();
+  var LokkaTransport = function () {
+    function LokkaTransport() {
+      (0, _classCallCheck3.default)(this, LokkaTransport);
     }
-    /*eslint-enable */
 
-  }]);
-  return LokkaTransport;
-}();
+    (0, _createClass3.default)(LokkaTransport, [{
+      key: 'send',
+      value: function send(rawQuery, variables, operationName) {
+        throw new Error('not implemented!');
+        // return new Promise();
+      }
+    }]);
+    return LokkaTransport;
+  }();
 
-exports.default = LokkaTransport;
+  exports.default = LokkaTransport;
+});
